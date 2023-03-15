df9aa381f22b5e0d442d1159078935369a94fa55
DucPhamH <ducphamhong2@gmail.com>
2023-03-15 19:37:42 +0700

    add, delete, edit post

--- src/Pages/Blog/Blog.reduce.ts
+++ src/Pages/Blog/Blog.reduce.ts
@@ -1,12 +1,59 @@
-import { createReducer } from '@reduxjs/toolkit'
+import { createAction, createReducer, current } from '@reduxjs/toolkit'
+import { initalPostList } from '../../constants/blog'
 import { Post } from '../../type/blog.type'
 
 interface BlogState {
   postList: Post[]
+  editingPost: Post | null
 }
 
 const initalState: BlogState = {
-  postList: []
+  postList: initalPostList,
+  editingPost: null
 }
-const blogReducer = createReducer(initalState, (builder) => {})
+
+export const addPost = createAction<Post>('blog/addPost')
+export const deletePost = createAction<string>('blog/deletePost')
+export const startEditingPost = createAction<string>('blog/startEditingPost')
+export const cancelEditingPost = createAction('blog/cancelEditingPost')
+export const finishEditingPost = createAction<Post>('blog/finishEditingPost')
+const blogReducer = createReducer(initalState, (builder) => {
+  builder
+    .addCase(addPost, (state, action) => {
+      const post = action.payload
+      state.postList.push(post)
+    })
+    .addCase(deletePost, (state, action) => {
+      const id = action.payload
+      const foundPostIndex = state.postList.findIndex((post) => post.id === id)
+      if (foundPostIndex !== -1) {
+        state.postList.splice(foundPostIndex, 1)
+      }
+    })
+    .addCase(startEditingPost, (state, action) => {
+      const id = action.payload
+      const foundPost = state.postList.find((post) => post.id === id) || null
+      state.editingPost = foundPost
+    })
+    .addCase(cancelEditingPost, (state) => {
+      state.editingPost = null
+    })
+    .addCase(finishEditingPost, (state, action) => {
+      const id = action.payload.id
+      state.postList.some((post, index) => {
+        if (post.id === id) {
+          state.postList[index] = action.payload
+          return true
+        }
+        return false
+      })
+      state.editingPost = null
+    })
+    .addMatcher(
+      (action) => action.type.includes('cancel'),
+      (state, action) => {
+        console.log(current(state))
+      }
+    )
+})
 export default blogReducer
--- src/Pages/Blog/Components/PostList/PostList.tsx
+++ src/Pages/Blog/Components/PostList/PostList.tsx
@@ -1,6 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../../../../Store'
+import { deletePost, startEditingPost } from '../../Blog.reduce'
+
 import PostItem from '../PostItem'
 
 export default function PostList() {
+  const postList = useSelector((state: RootState) => state.blog.postList)
+  const dispatch = useDispatch()
+  const handleDelete = (postId: string) => {
+    dispatch(deletePost(postId))
+  }
+  const handleStartEditing = (postId: string) => {
+    dispatch(startEditingPost(postId))
+  }
   return (
     <div>
       <div className='bg-white py-6 sm:py-8 lg:py-12'>
@@ -12,10 +24,9 @@ export default function PostList() {
             </p>
           </div>
           <div className='grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-2 xl:grid-cols-2 xl:gap-8'>
-            <PostItem />
-            <PostItem />
-            <PostItem />
-            <PostItem />
+            {postList.map((post) => (
+              <PostItem post={post} key={post.id} handleDelete={handleDelete} handleStartEditing={handleStartEditing} />
+            ))}
           </div>
         </div>
       </div>
